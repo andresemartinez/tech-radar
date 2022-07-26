0b685f64d3b8cb43471c03dcd16a26ca520204b5
Andres Martinez <andres93.lml@gmail.com>
2022-07-26 17:47:55 -0300

    Updating settings to use always use LF

--- src/pages/admin/technology/skill/level/[id].tsx
+++ src/pages/admin/technology/skill/level/[id].tsx
@@ -1,40 +1,42 @@
-import { useRouter } from "next/router";
-import { NextPageWithLayout } from "~/pages/_app";
+import { useRouter } from 'next/router';
+import { NextPageWithLayout } from '~/pages/_app';
 import { useMemo } from 'react';
-import { trpc } from "~/utils/trpc";
-import { TextField } from "@mui/material";
+import { trpc } from '~/utils/trpc';
+import { TextField } from '@mui/material';
 
 const TechnologySkillLevelAdminPage: NextPageWithLayout = () => {
+  const router = useRouter();
+  const id = useMemo(
+    () => (typeof router.query.id === 'string' ? router.query.id : ''),
+    [router.query.id],
+  );
 
-    const router = useRouter();
-    const id = useMemo(
-        () => (typeof router.query.id === 'string' ? router.query.id : ''),
-        [router.query.id],
-      );
-      
-      const { data: skillLevel } = trpc.useQuery(['technology-skill-level.byId', { id }]);
+  const { data: skillLevel } = trpc.useQuery([
+    'technology-skill-level.byId',
+    { id },
+  ]);
 
-      return (
-        <form>
-          <br />
-          <TextField
-            id="name"
-            name="name"
-            label="Nombre"
-            variant="outlined"
-            required
-            value={skillLevel?.name}
-          ></TextField>
-          <TextField
-            id="weight"
-            name="weight"
-            label="Peso"
-            variant="outlined"
-            required
-            value={skillLevel?.weight}
-          ></TextField>
-        </form>
-      );
-}
+  return (
+    <form>
+      <br />
+      <TextField
+        id="name"
+        name="name"
+        label="Nombre"
+        variant="outlined"
+        required
+        value={skillLevel?.name}
+      ></TextField>
+      <TextField
+        id="weight"
+        name="weight"
+        label="Peso"
+        variant="outlined"
+        required
+        value={skillLevel?.weight}
+      ></TextField>
+    </form>
+  );
+};
 
 export default TechnologySkillLevelAdminPage;
