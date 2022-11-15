0ccdbae8b33b6384a5c2da9c2ab8241f0258c425
Andres Martinez <andres93.lml@gmail.com>
2022-11-15 11:37:28 -0300

    Fixing tech radar form

--- src/pages/admin/tech-radar/index.tsx
+++ src/pages/admin/tech-radar/index.tsx
@@ -18,189 +18,23 @@ import { RouterInput, RouterOutput, trpc } from '~/utils/trpc';
 
 type TechRadarCreateMutation = RouterInput['techRadar']['create']['data'];
 type TechRadar = RouterOutput['techRadar']['all'][number];
-
-const formFieldByAngularAxisType: {
-  [key in TechRadarAngularAxisType]: keyof TechRadarCreateMutation;
-} = {
-  [TechRadarAngularAxisType.category]: 'techCategories',
-  [TechRadarAngularAxisType.technology]: 'technologies',
-};
-
-const formFieldByRadialAxisType: {
-  [key in Exclude<
-    TechRadarRadialAxisType,
-    'company'
-  >]: keyof TechRadarCreateMutation;
-} = {
-  [TechRadarRadialAxisType.professional]: 'professionals',
-};
+type Technology = RouterOutput['technology']['all'][number];
+type TechCategory = RouterOutput['techCategory']['all'][number];
+type Professional = RouterOutput['professional']['all'][number];
 
 const TechRadarAdminPage: NextPageWithLayout = () => {
-  const { data: session } = useSession();
-  const trpcUtils = trpc.useContext();
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const { data: professional } = trpc.professional.byUserId.useQuery(
-    {
-      userId: session?.user?.id ?? '',
-    },
-    { enabled: Boolean(session?.user?.id) },
-  );
   const { data: techRadars } = trpc.techRadar.all.useQuery();
-  const { data: technologies } = trpc.technology.all.useQuery();
-  const { data: categories } = trpc.techCategory.all.useQuery();
-  const { data: professionals } = trpc.professional.all.useQuery();
-
-  const createTechRadar = trpc.techRadar.create.useMutation({
-    async onSuccess() {
-      trpcUtils.techRadar.invalidate();
-    },
-  });
-
-  const { control, handleSubmit } = useForm<{
-    name: string;
-    technologies: { id: string; name: string }[];
-    owner: string;
-    angularAxis: TechRadarAngularAxisType;
-    radialAxis: TechRadarRadialAxisType;
-    professionals: { id: string; name: string }[];
-    techCategories: { id: string; name: string }[];
-  }>({
-    defaultValues: {
-      professionals: [],
-      techCategories: [],
-      technologies: [],
-    },
-  });
-
-  const angularAxisType = useWatch<TechRadarAngularAxisType>({
-    control,
-    name: 'angularAxis',
-  });
-  const radialAxisType = useWatch<TechRadarRadialAxisType>({
-    control,
-    name: 'radialAxis',
-  });
 
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col p-5">
         {techRadars?.map((techRadar) => (
           <TechRadarTableRow key={techRadar.id} techRadar={techRadar} />
         ))}
       </div>
-      <div>
-        <Button onClick={() => setModalOpen(true)}>Create Tech Radar</Button>
+      <div className="px-3">
+        <CreateTechRadarButton />
       </div>
-
-      <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-        <div className="w-[600px] px-[20px] py-[40px]">
-          <form
-            onSubmit={handleSubmit((data) => {
-              createTechRadar.mutateAsync({
-                data: {
-                  ...data,
-                  owner: professional?.id ?? '',
-                  professionals: data.professionals.map(
-                    (professional) => professional.id,
-                  ),
-                  technologies: data.technologies.map(
-                    (technology) => technology.id,
-                  ),
-                  techCategories: data.techCategories.map(
-                    (techCategory) => techCategory.id,
-                  ),
-                },
-              });
-            })}
-          >
-            <div className="flex flex-col">
-              <div className="flex flex-row">
-                <TextInput name="name" control={control} required />
-              </div>
-
-              <div className="flex flex-row">
-                <div className="flex flex-col pr-2 pl-2 flex-grow">
-                  <Autocomplete
-                    name="angularAxis"
-                    label="Angular Axis"
-                    control={control}
-                    required
-                    options={Object.values(TechRadarAngularAxisType)}
-                    getOptionLabel={(option) =>
-                      `${option
-                        .substring(0, 1)
-                        .toUpperCase()}${option.substring(1)}`
-                    }
-                    isOptionEqualToValue={(option, value) => option === value}
-                  />
-
-                  {categories !== undefined &&
-                    technologies !== undefined &&
-                    angularAxisType !== undefined && (
-                      <Autocomplete
-                        name={formFieldByAngularAxisType[angularAxisType]}
-                        label="Blah"
-                        control={control}
-                        required
-                        multiple
-                        options={
-                          angularAxisType === TechRadarAngularAxisType.category
-                            ? categories
-                            : technologies
-                        }
-                        getOptionLabel={(option) => option.name ?? ''}
-                        isOptionEqualToValue={(option, value) =>
-                          option.id === value.id
-                        }
-                      />
-                    )}
-                </div>
-
-                <div className="flex flex-col pr-2 pl-2 flex-grow">
-                  <Autocomplete
-                    name="radialAxis"
-                    label="Radial Axis"
-                    control={control}
-                    required
-                    options={Object.values(TechRadarRadialAxisType)}
-                    getOptionLabel={(option) =>
-                      `${option
-                        .substring(0, 1)
-                        .toUpperCase()}${option.substring(1)}`
-                    }
-                    isOptionEqualToValue={(option, value) => option === value}
-                  />
-
-                  {professionals !== undefined &&
-                    radialAxisType !== undefined &&
-                    radialAxisType !== TechRadarRadialAxisType.company && (
-                      <Autocomplete
-                        name={formFieldByRadialAxisType[radialAxisType]}
-                        label="Professionals"
-                        control={control}
-                        required
-                        multiple
-                        options={professionals}
-                        getOptionLabel={(option) => option.name ?? ''}
-                        isOptionEqualToValue={(option, value) =>
-                          option.id === value.id
-                        }
-                      />
-                    )}
-                </div>
-              </div>
-            </div>
-
-            <div className="flex justify-end pt-5">
-              <Button className="pr-3" onClick={() => setModalOpen(false)}>
-                Cancel
-              </Button>
-              <Button type="submit">Save</Button>
-            </div>
-          </form>
-        </div>
-      </Modal>
     </>
   );
 };
@@ -220,8 +54,8 @@ const TechRadarTableRow = ({ techRadar }: TechRadarTableRowProps) => {
   return (
     <>
       <div className="flex flex-row" onClick={() => setModalOpen(true)}>
-        <div>{techRadar.name}</div>
-        <div>{techRadar.owner}</div>
+        <div className="pr-2">{techRadar.name}</div>
+        <div className="pl-2">{techRadar.owner}</div>
       </div>
 
       <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
@@ -230,6 +64,212 @@ const TechRadarTableRow = ({ techRadar }: TechRadarTableRowProps) => {
         </div>
       </Modal>
     </>
+  );
+};
+
+const CreateTechRadarButton = () => {
+  const trpcUtils = trpc.useContext();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const { data: technologies } = trpc.technology.all.useQuery();
+  const { data: techCategories } = trpc.techCategory.all.useQuery();
+  const { data: professionals } = trpc.professional.all.useQuery();
+
+  const createTechRadar = trpc.techRadar.create.useMutation({
+    async onSuccess() {
+      trpcUtils.techRadar.invalidate();
+    },
+  });
+
+  return (
+    <>
+      <div>
+        <Button onClick={() => setModalOpen(true)}>Create Tech Radar</Button>
+      </div>
+
+      <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+        <div className="w-[600px] px-[20px] py-[40px]">
+          <CreateTechRadarForm
+            technologies={technologies ?? []}
+            techCategories={techCategories ?? []}
+            professionals={professionals ?? []}
+            onCancel={() => setModalOpen(false)}
+            onSubmit={(techRadar) => {
+              createTechRadar.mutateAsync({ data: techRadar });
+              setModalOpen(false);
+            }}
+          />
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+type CreateTechRadarFormProps = {
+  technologies: Technology[];
+  techCategories: TechCategory[];
+  professionals: Professional[];
+  onCancel: () => void;
+  onSubmit: (techRadar: TechRadarCreateMutation) => void;
+};
+
+const CreateTechRadarForm = ({
+  technologies,
+  techCategories,
+  professionals,
+  onCancel,
+  onSubmit,
+}: CreateTechRadarFormProps) => {
+  const { data: session } = useSession();
+  const { data: professional } = trpc.professional.byUserId.useQuery(
+    {
+      userId: session?.user?.id ?? '',
+    },
+    { enabled: Boolean(session?.user?.id) },
+  );
+
+  const { control, handleSubmit } = useForm<{
+    name: string;
+    technologies: { id: string; name: string }[];
+    angularAxis: TechRadarAngularAxisType;
+    radialAxis: TechRadarRadialAxisType;
+    professionals: { id: string; name: string }[];
+    techCategories: { id: string; name: string }[];
+  }>({
+    defaultValues: {
+      name: '',
+      professionals: [],
+      techCategories: [],
+      technologies: [],
+    },
+  });
+
+  const angularAxisType = useWatch({
+    control,
+    name: 'angularAxis',
+  });
+  const radialAxisType = useWatch({
+    control,
+    name: 'radialAxis',
+  });
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) =>
+        onSubmit({
+          ...data,
+          owner: professional?.id ?? '',
+          technologies: data.technologies.map((tech) => tech.id),
+          techCategories: data.techCategories.map(
+            (techCategory) => techCategory.id,
+          ),
+          professionals: data.professionals.map(
+            (professional) => professional.id,
+          ),
+        }),
+      )}
+    >
+      <div className="flex flex-col">
+        <div className="flex flex-row pb-2 flex-grow">
+          <TextInput name="name" label="Name" control={control} required />
+        </div>
+
+        <div className="flex flex-row pt-2 flex-grow">
+          <div className="flex flex-col pr-2 flex-grow">
+            <Autocomplete
+              className="pb-2"
+              name="angularAxis"
+              label="Angular Axis"
+              control={control}
+              required
+              options={Object.values(TechRadarAngularAxisType)}
+              getOptionLabel={(option) =>
+                option
+                  ? `${option.substring(0, 1).toUpperCase()}${option.substring(
+                      1,
+                    )}`
+                  : ''
+              }
+              isOptionEqualToValue={(option, value) => option === value}
+            />
+
+            <Autocomplete
+              className={`pt-2 ${
+                angularAxisType === TechRadarAngularAxisType.category
+                  ? ''
+                  : 'hidden'
+              }`}
+              name="techCategories"
+              label="Categories"
+              control={control}
+              required={angularAxisType === TechRadarAngularAxisType.category}
+              multiple
+              options={techCategories}
+              getOptionLabel={(option) => option.name ?? ''}
+              isOptionEqualToValue={(option, value) => option.id === value.id}
+            />
+
+            <Autocomplete
+              className={`pt-2 ${
+                angularAxisType === TechRadarAngularAxisType.technology
+                  ? ''
+                  : 'hidden'
+              }`}
+              name="technologies"
+              label="Technologies"
+              control={control}
+              required={angularAxisType === TechRadarAngularAxisType.technology}
+              multiple
+              options={technologies}
+              getOptionLabel={(option) => option.name ?? ''}
+              isOptionEqualToValue={(option, value) => option.id === value.id}
+            />
+          </div>
+
+          <div className="flex flex-col pl-2 flex-grow">
+            <Autocomplete
+              className="pb-2"
+              name="radialAxis"
+              label="Radial Axis"
+              control={control}
+              required
+              options={Object.values(TechRadarRadialAxisType)}
+              getOptionLabel={(option) =>
+                option
+                  ? `${option.substring(0, 1).toUpperCase()}${option.substring(
+                      1,
+                    )}`
+                  : ''
+              }
+              isOptionEqualToValue={(option, value) => option === value}
+            />
+
+            <Autocomplete
+              className={`pt-2 ${
+                radialAxisType === TechRadarRadialAxisType.professional
+                  ? ''
+                  : 'hidden'
+              }`}
+              name="professionals"
+              label="Professionals"
+              control={control}
+              required={radialAxisType === TechRadarRadialAxisType.professional}
+              multiple
+              options={professionals}
+              getOptionLabel={(option) => option.name ?? ''}
+              isOptionEqualToValue={(option, value) => option.id === value.id}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-end pt-5">
+        <Button className="pr-3" onClick={onCancel}>
+          Cancel
+        </Button>
+        <Button type="submit">Save</Button>
+      </div>
+    </form>
   );
 };
 
